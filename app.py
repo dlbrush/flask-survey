1ceb6a651c13fcc72a4bc1c4daa9f40aa48cfadd
Devlin Brush <devlinbrush@gmail.com>
2021-02-03 09:11:14 -0600

    converted to session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,12 +7,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thekey'
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 @app.route('/')
 def show_instructions():
     "Show the instruction page"
     return render_template('instructions.html', instructions=satisfaction_survey.instructions, title=satisfaction_survey.title)
+
+@app.route('/start', methods=["POST"])
+def start_survey():
+    "Initialize the response list and redirect the user to the start of the survey."
+    session['responses'] = []
+    return redirect('/questions/0')
 
 @app.route('/questions/<int:question_index>')
 def show_question(question_index):
@@ -21,13 +25,13 @@ def show_question(question_index):
     If the user has already completed the survey, redirect to the thanks page.
     If the user is trying to go to question page out of order redirect to the earliest question they haven't answered.
     """
-    if len(responses) == len(satisfaction_survey.questions):
+    if len(session['responses']) == len(satisfaction_survey.questions):
         flash("No need to answer any more questions (so don't try)!")
         return redirect('/thanks')
 
-    if question_index != len(responses):
+    if question_index != len(session['responses']):
         flash('Whoops! You skipped some questions. Start here.')
-        return redirect(f'/questions/{len(responses)}')
+        return redirect(f'/questions/{len(session["responses"])}')
 
     question = satisfaction_survey.questions[question_index].question
     choices = satisfaction_survey.questions[question_index].choices
@@ -41,8 +45,12 @@ def save_answer():
     Send to the next question if there are more questions to answer.
     Otherwise, send to the thanks page.
     """
+    responses = session['responses']
     responses.append(request.form['answer'])
+    session['responses'] = responses
+
     next_question = len(responses)
+
     if next_question < len(satisfaction_survey.questions):
         return redirect(f'/questions/{next_question}')
     else:
